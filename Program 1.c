5e7c3b87fae5f779ba7ee7f0a57997bd569fc03d
madhumitha-eng <madhumithaanjel@gmail.com>
2026-02-17 11:32:36 +0530

    Create Program 1.c

---
+++ Program 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() 
+{
+int m1, m2, m3, m4, m5, m6;
+scanf("%d%d%d%d%d%d", &m1, &m2, &m3, &m4, &m5, &m6) ;
+float percentage;
+if(percentage>=95) {
+printf("A") ;
+}
+else if(percentage<=&94&percentage>=85) {
+printf("B") ;
+}
+else if(percentage<=84&&percentage>=75) {
+printf("C") ;
+}
+else if(percentage<=74&&percentage>=65) {
+printf("D") ;
+}
+else if(percentage<=64&&percentage>=45) {
+printf("E") ;
+}
+else  {
+printf("F") ;
+}
