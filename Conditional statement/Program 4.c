b0731a611ec998009f8ec5de10df8cb07433cd58
madhumitha-eng <madhumithaanjel@gmail.com>
2026-02-18 13:31:06 +0530

    Add switch statement for day of the week

---
+++ Conditional statement/Program 4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int day;
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+    switch(day) {
+        case 1:
+            printf("Day: Monday\n");
+            printf("It is a Weekday.\n");
+            break;
+        case 2:
+            printf("Day: Tuesday\n");
+            printf("It is a Weekday.\n");
+            break;
+        case 3:
+            printf("Day: Wednesday\n");
+            printf("It is a Weekday.\n");
+            break;
+        case 4:
+            printf("Day: Thursday\n");
+            printf("It is a Weekday.\n");
+            break;
+        case 5:
+            printf("Day: Friday\n");
+            printf("It is a Weekday.\n");
+            break;
+        case
