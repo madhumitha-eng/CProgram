c2c5bf787415412358b2d7d4c81541d108b73769
madhumitha-eng <madhumithaanjel@gmail.com>
2026-02-19 12:18:47 +0530

    Create Program 44.c

---
+++ Conditional statement/Program 44.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float amount, discount = 0;
+
+    printf("Enter Purchase Amount: ");
+    scanf("%f", &amount);
+
+    if (amount >= 5000)
+        discount = amount * 0.20;
+    else if (amount >= 2000)
+        discount = amount * 0.10;
+    else if (amount >= 1000)
+        discount = amount * 0.05;
+    else
+        discount = 0;
+
+    printf("Discount Amount: %.2f\n", discount);
+
+    return 0;
+}
